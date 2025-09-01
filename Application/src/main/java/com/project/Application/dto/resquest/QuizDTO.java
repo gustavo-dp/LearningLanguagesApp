c522a2d3c6665b4d3c5baa32b939da7b33a423d0
Gustavo Deda <gustavo.deda687@gmail.com>
2025-09-01 15:45:18 -0300

    feat: adding quiz dto's

--- Application/src/main/java/com/project/Application/dto/resquest/QuizDTO.java
+++ Application/src/main/java/com/project/Application/dto/resquest/QuizDTO.java
@@ -1,4 +1,17 @@
 package com.project.Application.dto.resquest;
 
+import com.project.Application.entity.Quiz;
+
 public class QuizDTO {
+    private String title;
+    private String description;
+    private String difficultyLevel;
+
+    public Quiz toEntity(){
+        Quiz quiz = new Quiz();
+        quiz.setTitle(this.title);
+        quiz.setDescription(this.description);
+        quiz.setDifficultyLevel(this.difficultyLevel);
+        return quiz;
+    }
 }
