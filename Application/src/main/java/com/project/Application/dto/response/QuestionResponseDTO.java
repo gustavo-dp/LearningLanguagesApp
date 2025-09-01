0148eede059cedd4c9b7d31dca9ed975ff660303
Gustavo Deda <gustavo.deda687@gmail.com>
2025-09-01 15:39:25 -0300

    feat: adding questions dto's

--- Application/src/main/java/com/project/Application/dto/response/QuestionResponseDTO.java
+++ Application/src/main/java/com/project/Application/dto/response/QuestionResponseDTO.java
@@ -1,4 +1,25 @@
 package com.project.Application.dto.response;
 
+import com.project.Application.dto.resquest.UserAnswerDTO;
+import com.project.Application.entity.Question;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class QuestionResponseDTO {
+    private Long id;
+    private QuizResponseDTO quiz;
+    private List<OptionResponseDTO> options;
+    private List<UserAnswerResponseDTO> userAnswers;
+    private String text;
+    private String explanation;
+
+    public QuestionResponseDTO(Question question) {
+        this.id = question.getId();
+        this.quiz = new QuizResponseDTO(question.getQuiz());
+        this.options = question.getOptions().stream().map(OptionResponseDTO::new).toList();
+        this.userAnswers = question.getUserAnswers().stream().map(UserAnswerResponseDTO::new).toList();
+        this.text = question.getText();
+        this.explanation = question.getExplanation();
+    }
 }
